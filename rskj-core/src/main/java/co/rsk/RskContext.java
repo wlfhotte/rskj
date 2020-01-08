714a9c9b4193269b9538798a7b7c2104600464ec
ajlopez <webmaster@ajlopez.com>
2020-01-08 10:37:47 -0300

    Avoid send NEW_BLOCK_HASH for known blocks

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -1410,7 +1410,8 @@ public class RskContext implements NodeBootstrapper {
                 syncProcessor = new AlternativeSyncProcessorImpl(
                     getBlockchain(),
                     syncConfiguration,
-                    getPeersInformation()
+                    getPeersInformation(),
+                    getBlockNodeInformation()
                 );
             }
             else {
